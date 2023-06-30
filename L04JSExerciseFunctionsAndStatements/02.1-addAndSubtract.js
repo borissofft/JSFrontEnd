60ac862823eeec6b9c415b896675ace419b930f9
borissofft <kolegata86@abv.bg>
2023-06-30 21:41:53 +0300

    edit task 2

--- L04JSExerciseFunctionsAndStatements/02.1-addAndSubtract.js
+++ L04JSExerciseFunctionsAndStatements/02.1-addAndSubtract.js
@@ -1,13 +1,13 @@
 function addAndSubtract(num1, num2, num3) {
 
     function sum() {
-        return Number(num1) + Number(num2);
+        return num1 + num2;
     }
 
     let sumOfItems = sum();
 
     function subtract() {
-        return sumOfItems - Number(num3);
+        return sumOfItems - num3;
     }
 
     let result = subtract();
