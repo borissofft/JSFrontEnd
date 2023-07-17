0f3fc5ed41e4d161b6ec2528c6244c68cbe33c4a
borissofft <kolegata86@abv.bg>
2023-07-18 00:20:31 +0300

    tasks 4 and 5 are done

--- L08JSExerciseDOMAndEvents/05. Fill-Dropdown/dropdown.js
+++ L08JSExerciseDOMAndEvents/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,25 @@
 function addItem() {
-    console.log('TODO:...');
+
+    const textInput = document.querySelector("#newItemText");
+    const valueInput = document.querySelector("#newItemValue");
+
+    const option = createOption(textInput.value, valueInput.value);
+
+    const select = document.querySelector("#menu");
+    select.appendChild(option);
+    clearInputFields(textInput, valueInput);
+
+    function createOption(text, value) {
+        const option = document.createElement('option');
+        option.textContent = text;
+        option.setAttribute("value", value);
+
+        return option;
+    }
+
+    function clearInputFields() {
+        textInput.value = "";
+        valueInput.value = "";
+    }
+
 }
--- L08JSExerciseDOMAndEvents/06. Table - Search Engine/solution.js
+++ L08JSExerciseDOMAndEvents/06. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+
+      const searchQuery = document.querySelector("#searchField").value;
+      const cels = Array.from(document.querySelectorAll("tbody td"));
+
+      cels.forEach(cell => {
+         if (cell.textContent.includes(searchQuery)) {
+            
+         }
+      })
 
    }
 }
