9fc3b580388d93bf4e36f338368acb69480c5538
borissofft <kolegata86@abv.bg>
2023-07-23 11:27:36 +0300

    task 1 is done

--- L10JSExerciseHTTPandREST/01.Bus-Stop/app.js
+++ L10JSExerciseHTTPandREST/01.Bus-Stop/app.js
@@ -1,3 +1,22 @@
 function getInfo() {
-    // TODO:
+
+    const busStopID = document.querySelector("#stopId").value;
+    const list = document.querySelector("ul");
+    list.innerHTML = "";
+    const stopName = document.querySelector("#stopName");
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopID}`)
+        .then(res => res.json())
+        .then(busStop => {
+            stopName.textContent = busStop.name;
+
+            Object.entries(busStop.buses).map(([busNumber, timeInMinutes]) => {
+                const item = document.createElement('li');
+                item.textContent = `Bus ${busNumber} arrives in ${timeInMinutes} minutes`;
+                list.appendChild(item);
+            })
+        })
+        .catch(() => {
+            stopName.textContent = "Error";
+        });
 }
