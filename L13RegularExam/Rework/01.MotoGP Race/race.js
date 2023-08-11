357063371a8b515bfcae7e47f4cf1d01b27e1090
borissofft <kolegata86@abv.bg>
2023-08-12 00:34:22 +0300

    edit of the input data

--- L13RegularExam/Rework/01.MotoGP Race/race.js
+++ L13RegularExam/Rework/01.MotoGP Race/race.js
@@ -59,10 +59,11 @@ const input = (["3",
 "Valentino Rossi|100|1",
 "Marc Marquez|90|2",
 "Jorge Lorenzo|80|3",
-"StopForFuel - Valentino Rossi -50 - 1",
+"StopForFuel - Valentino Rossi - 50 - 1",
 "Overtaking - Marc Marquez - Jorge Lorenzo",
 "EngineFail - Marc Marquez - 10",
 "Finish"])
+
 
 
 
