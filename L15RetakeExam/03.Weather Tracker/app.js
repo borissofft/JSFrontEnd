e13ae1609683c9e5198a8b2d180d179c2a9d1778
borissofft <kolegata866@abv.bg>
2023-08-17 12:58:09 +0300

    task 3 final solution 50/100

--- L15RetakeExam/03.Weather Tracker/app.js
+++ L15RetakeExam/03.Weather Tracker/app.js
@@ -11,6 +11,7 @@ const inputSelectors = {
 const selectors = {
     loadButton: document.querySelector("#load-history"),
     addButton: document.querySelector("#add-weather"),
+    editButton: document.querySelector("#edit-weather"),
     mainDivContainer: document.querySelector("#list"),
 };
 
@@ -27,19 +28,63 @@ async function loadTasks() {
     selectors.mainDivContainer.innerHTML = "";
 
     tasks = await (await fetch(API_URL)).json();
-    // console.log(JSON.stringify(tasks, 0, 2));
+    console.log(JSON.stringify(tasks, 0, 2));
 
     Object.values(tasks).forEach(task => {
 
-        const div = createElement("div", null, ["container"], task.id);
+        const div = createElement("div", null, ["container"], task._id);
         createElement("h2", task.location, [], null, div);
         createElement("h3", task.date, [], null, div);
         createElement("h2", task.temperature, [], "celsius", div);
 
         const divButtons = createElement("div", null, ["buttons-container"], null, div);
 
-        const changeButton = createElement("button", "Change", ["change-btn"], task.id, divButtons);
-        const deleteButton = createElement("button", "Delete", ["delete-btn"], task.id, divButtons);
+        const changeButton = createElement("button", "Change", ["change-btn"], task._id, divButtons);
+        changeButton.addEventListener("click", async (e) => {
+
+            Object.keys(inputSelectors).forEach(key => {
+                const selector = inputSelectors[key];
+                selector.value = tasks[task._id][key];
+            });
+
+            e.currentTarget.parentElement.parentElement.remove();
+
+            selectors.editButton.disabled = false;
+            selectors.addButton.disabled = true;
+
+            selectors.editButton.addEventListener("click", async (e) => {
+
+                const taskToEdit = {
+                    location: inputSelectors.location.value,
+                    temperature: inputSelectors.temperature.value,
+                    date: inputSelectors.date.value,
+                    _id: task._id,
+                }
+
+                await fetch(`${API_URL}/${task._id}`, {
+                    method: "PUT",
+                    body: JSON.stringify(taskToEdit),
+                });
+
+                Object.values(inputSelectors).forEach(selector => selector.value = "");
+                selectors.editButton.disabled = true;
+                selectors.addButton.disabled = false;
+                selectors.mainDivContainer.innerHTML = "";
+                loadTasks(e);
+
+            });
+
+        });
+
+
+        const deleteButton = createElement("button", "Delete", ["delete-btn"], task._id, divButtons);
+        deleteButton.addEventListener("click", async (e) => {
+            await fetch(`${API_URL}/${task._id}`, {
+                method: "DELETE",
+            });
+
+            await loadTasks();
+        });
 
         selectors.mainDivContainer.appendChild(div);
 
